3e24ce4241b974a20334055965604def13baa499
NitroSniper <NitroSniper@users.noreply.github.com>
2022-11-25 13:41:56 +0000

    created BoilerPlate and starting work on Messier. To be finished in labs
    Co-authored-by: LividJaffas <LividJaffas@users.noreply.github.com>

---
+++ lib/MessierObject.java
@@ -0,0 +1,24 @@
+public class MessierObject {
+
+    enum NGCIC {
+        
+    }
+
+
+    int messierNumber;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    public static void main(String[] args) {
+        
+    }
+}
