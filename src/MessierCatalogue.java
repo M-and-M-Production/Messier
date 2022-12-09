1de560d50b90627e938b7e36c0c0a2e9bd71b495
Nitro Sniper <44097331+NitroSniper@users.noreply.github.com>
2022-12-09 11:59:06 +0000

    added Comments

--- src/MessierCatalogue.java
+++ src/MessierCatalogue.java
@@ -10,24 +10,84 @@ public class MessierCatalogue {
     }
     
     }
-    // basic functions to add and remove objects
+    /**
+     * Add an object to the catalogue
+     * Example:
+     * <blockquote><pre>
+     * MessierCatalogue catalogue = new MessierCatalogue();
+     * MessierObject object = new MessierObject();
+     * catalogue.addObject(object);
+     * </pre></blockquote>
+     * @param object the object to add to the catalogue
+     * @return void
+     * @see MessierObject
+     */
     public void addObject(MessierObject object) {
         collection.add(object);
     }
+
+    /**
+     * Remove an object from the catalogue
+     * Example:
+     * <blockquote><pre>
+     * MessierCatalogue catalogue = new MessierCatalogue();
+     * MessierObject object = new MessierObject();
+     * catalogue.addObject(object);
+     * catalogue.removeObject(object);
+     * </pre></blockquote>
+     * @param object the object to remove from the catalogue
+     * @return void
+     * @see MessierObject
+     */
     public void removeObject(MessierObject object) {
         collection.remove(object);
     }
-    // print all objects in the catalogue
+    /**
+     * Print all objects in the catalogue
+     * Example:
+     * <blockquote><pre>
+     * MessierCatalogue catalogue = new MessierCatalogue();
+     * MessierObject object = new MessierObject();
+     * catalogue.addObject(object);
+     * catalogue.printAll();
+     * </pre></blockquote>
+     * @return void
+     * @see MessierObject
+     * @see MessierObject#toString()
+     */
     public void printAll() {
         for (MessierObject object : collection) {
-            System.out.println(object);
+            System.out.println(object.toString());
         }
     };
-    // sort the objects by their magnitude with compareTo
+    /**
+     * Sort the catalogue by the magnitude of the objects
+     * Example:
+     * <blockquote><pre>
+     * MessierCatalogue catalogue = new MessierCatalogue();
+     * MessierObject object = new MessierObject();
+     * catalogue.addObject(object);
+     * catalogue.sortByMagnitude();
+     * </pre></blockquote>
+     * @return void
+     * @see MessierObject
+     */
     public void sortByMagnitude() {
         collection.sort(null);
     }
-    // query the collection for matching fields
+    /**
+     * Filter the catalogue by a query
+     * Example:
+     * <blockquote><pre>
+     * MessierCatalogue catalogue = new MessierCatalogue();
+     * MessierObject object = new MessierObject();
+     * catalogue.addObject(object);
+     * catalogue.match("{\"type\": \"GALAXY\"}");
+     * </pre></blockquote>
+     * @param json the query to filter the catalogue by
+     * @return MessierCatalogue
+     * @see MessierObject
+     */
     public void match(String json) {
         // convert the json string to a JSONObject
         JSONObject query = new JSONObject(json);
